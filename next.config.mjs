671e7b3dfd2ad709618f827391bb59ac9ee48bb6
Walid <chromadreamstudio@gmail.com>
2026-08-29 08:57:42 +0300

    force ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // تجاهل أخطاء ESLint أثناء الرفع
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // تجاهل أخطاء TypeScript أثناء الرفع
     ignoreBuildErrors: true,
   },
 };
-
-export default nextConfig;
+module.exports = nextConfig;
